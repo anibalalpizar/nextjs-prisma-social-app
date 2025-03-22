bfaa6e2c91b5181c1b575bd507fb4aa44d5fec3a
anibalalpizar <anibal.alpizar14@gmail.com>
2025-03-21 23:14:57 -0600

    add Sidebar component to RootLayout for improved UI structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { ClerkProvider } from "@clerk/nextjs"
 
 import { ThemeProvider } from "@/components/theme-provider"
 import Navbar from "@/components/Navbar"
+import Sidebar from "@/components/Sidebar"
 
 import "./globals.css"
 
@@ -45,7 +46,9 @@ export default function RootLayout({
               <main className="py-8">
                 <div className="max-w-7xl mx-auto px-4">
                   <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
-                    <div className="hidden lg:block lg:col-span-3">Sidebar</div>
+                    <div className="hidden lg:block lg:col-span-3">
+                      <Sidebar />
+                    </div>
                     <div className="lg:col-span-9">{children}</div>
                   </div>
                 </div>
