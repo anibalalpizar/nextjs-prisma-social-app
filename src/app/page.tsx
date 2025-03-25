084b4dd1a75132a168ed398ef808ee50e8357da0
anibalalpizar <anibal.alpizar14@gmail.com>
2025-03-24 20:55:50 -0600

    uncomment WhoToFollow component in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { currentUser } from "@clerk/nextjs/server"
 
 import CreatePost from "@/components/CreatePost"
+import WhoToFollow from "@/components/WhoToFollow"
 
 
 async function Home() {
@@ -9,7 +10,7 @@ async function Home() {
     <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
       <div className="lg:col-span-6">{user ? <CreatePost /> : null}</div>
       <div className="hidden lg:block lg:col-span-4 sticky top-20">
-        {/* <WhoToFollow /> */}
+      <WhoToFollow />
       </div>
     </div>
   )
