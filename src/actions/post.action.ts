154eca718659f9eca14ffbc4fd51bd01b9db5614
anibalalpizar <anibal.alpizar14@gmail.com>
2025-03-25 22:54:35 -0600

    fix: ensure user is authenticated before creating a post

--- src/actions/post.action.ts
+++ src/actions/post.action.ts
@@ -7,6 +7,7 @@ import { getUserId } from "./user.action"
 export async function createPost(content: string, image: string) {
   try {
     const userId = await getUserId()
+    if (!userId) return
 
     const post = await prisma.post.create({
       data: {
@@ -20,5 +21,54 @@ export async function createPost(content: string, image: string) {
     return { success: true, post }
   } catch (error) {
     return { success: false, error: `Failed to create post: ${error}` }
+  }
+}
+
+export async function getPosts() {
+  try {
+    const posts = await prisma.post.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+      include: {
+        author: {
+          select: {
+            name: true,
+            image: true,
+            username: true,
+          },
+        },
+        comments: {
+          include: {
+            author: {
+              select: {
+                id: true,
+                username: true,
+                image: true,
+                name: true,
+              },
+            },
+          },
+          orderBy: {
+            createdAt: "asc",
+          },
+        },
+        likes: {
+          select: {
+            userId: true,
+          },
+        },
+        _count: {
+          select: {
+            likes: true,
+            comments: true,
+          },
+        },
+      },
+    })
+    return posts
+  } catch (error) {
+    console.error("Error fetching posts:", error)
+    throw new Error("Failed to fetch posts")
   }
 }
